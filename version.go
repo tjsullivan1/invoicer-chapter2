9d20d22bd7c2b3076c4573b72ffec26960933e72
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-07-08 23:09:13 +0000

    updated version script to use correct line endings and ran it to update version

--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
-const version = "20180614.0-92f47c3"
-const commit = "92f47c3d3dd24e41c8b38adbb86b2407143c9dc9"
+const version = "20200708.0-ee37172"
+const commit = "ee37172398c8076591ca889af45bd4a6f76c4b7d"
